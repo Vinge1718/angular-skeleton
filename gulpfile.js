22b3ca96255d0d8b8d3cc3ef641fa54e782de606
Victor King'e <vinge1718@gmail.com>
2017-09-20 17:09:55 +0300

    add the angular js skeleton

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var lib = require("bower-files")({
+  "overrides":{
+    "bootstrap":{
+      "main":[
+        "less/bootstrap.less",
+        "dist/css/bootstrap.min.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+var utilities = require("gulp-util");
+var buildProduction = utilities.env.production;
+var del = require("del");
+var browserSync = require("browser-sync").create();
+var shell = require("gulp-shell");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("build", ["ts"], function(){
+  gulp.start("bower");
+  gulp.start("sassBuild")
+});
+
+//typescript
+gulp.task("tsClean", function(){
+  return del(["app/*.js", "app/*.js.map"]);
+});
+gulp.task("ts", ["tsClean"], shell.task(["tsc"]));
+
+//Bower
+gulp.task("jsBowerClean", function(){
+  return del(["./build/js/vendor.min.js"]);
+});
+gulp.task("jsBower",["jsBowerClean"], function(){
+  return gulp.src(lib.ext("js").files)
+    .pipe(concat("vendor.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+gulp.task("cssBowerClean", function(){
+  return del(["./build/css/vendor.css"]);
+});
+gulp.task("cssBower", ["cssBowerClean"], function(){
+  return gulp.src(lib.ext("css").files)
+  .pipe(concat("vendor.css"))
+  .pipe(gulp.dest("./build/css"));
+});
+gulp.task("bower", ["jsBower", "cssBower"]);
+
+//sass
+gulp.task("sassBuild", function(){
+  return gulp.src(["resources/styles/*.scss"])
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest("./build/css"));
+});
+
+//server
+gulp.task("jsbuild", function(){
+  browserSync.reload();
+});
+gulp.task("htmlBuild", function(){
+  browserSync.reload();
+});
+gulp.task("cssBuild", function(){
+  browserSync.reload();
+});
+gulp.task("tsBuild", function(){
+  browserSync.reload();
+});
+gulp.task("serve", ["build"], function(){
+  browserSync.init({
+    server:{
+      baseDir:"./",
+      index:"index.html"
+    }
+  });
+  gulp.watch(["resources/js/*.js"], ["jsBuild"]);
+  gulp.watch(["*.html"], ["htmlBuild"]);
+  gulp.watch(["resources/styles/*.css", "resources/styles/*.scss"], ["cssBuild"]);
+  gulp.watch(["app/*.ts"], ["tsBuild"]);
+});
